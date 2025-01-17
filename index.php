ec25dae3cd820a893e37b34c452601a234b1696e
MaximeHenault <maxime.henault@lyceefulbert.fr>
2025-01-17 16:08:20 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,34 +1,10 @@
 <?php
-    echo "<h1>Bonjour ! Bienvenue sur mon php !</h1>";
-    echo "<h2>Voila mon Cluedo :</h2>";
-    echo "<h3>Les armes :</h3>";
-    echo "<ul>
-    <li>Couteau</li>
-    <li>Revolver</li>
-    <li>Chandelle</li>
-    <li>Corde</li>
-    <li>Clé anglaise</li>
-    <li>Barre de fer</li>
-    </ul>";
-    echo "<h3>Les personnages :</h3>";
-    echo "<ul>
-    <li>Madame Leblanc</li>
-    <li>Monsieur Olive</li>
-    <li>Professeur Violet</li>
-    <li>Colonnel Moutarde</li>
-    <li>Mademoiselle Rose</li>
-    <li>Docteur Pervenche</li>
-    </ul>";
-    echo "<h3>Les pièces :</h3>";
-    echo "<ul>
-    <li>Hall</li>
-    <li>Salon</li>
-    <li>Salle à manger</li>
-    <li>Bibliothèque</li>
-    <li>Véranda</li>
-    <li>Cuisine</li>
-    <li>Salle de billard</li>
-    <li>Bureau</li>
-    </ul>";
+
+    include('appmvc.php');
+    $mavar = new Maclasse();
+    echo $mavar -> Getmonattribut();
+    $mavar -> Setmonattribut(5);
+    echo "Oui";
+    echo $mavar -> Getmonattribut();
 
 ?>
