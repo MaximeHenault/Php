a36c5fe4697288747b5d5fe459c07be0fca04efb
MaximeHenault <maxime.henault@lyceefulbert.fr>
2025-01-17 15:08:28 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+    echo "<h1>Bonjour ! Bienvenue sur mon php !</h1>";
+    echo "<h2>Voila mon Cluedo :</h2>";
+    echo "<h3>Les armes :</h3>";
+    echo "<ul>
+    <li>Couteau</li>
+    <li>Revolver</li>
+    <li>Chandelle</li>
+    <li>Corde</li>
+    <li>Clé anglaise</li>
+    </ul>";
+    echo "<h3>Les personnages :</h3>";
+    echo "<ul>
+    <li>Madame Leblanc</li>
+    <li>Monsieur Olive</li>
+    <li>Professeur Violet</li>
+    <li>Colonnel Moutarde</li>
+    <li>Mademoiselle Rose</li>
+    <li>Docteur Pervenche</li>
+    </ul>";
+    echo "<h3>Les pièces :</h3>";
+    echo "<ul>
+    <li>Hall</li>
+    <li>Salon</li>
+    <li>Salle à manger</li>
+    <li>Bibliothèque</li>
+    <li>Véranda</li>
+    <li>Cuisine</li>
+    </ul>";
+
+?>
