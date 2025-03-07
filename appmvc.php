d789ed04eed7294fc97bb04533f591ae06c20066
MaximeHenault <maxime.henault@lyceefulbert.fr>
2025-03-07 15:17:40 +0100

    Update appmvc.php

--- appmvc.php
+++ appmvc.php
@@ -1,41 +1,87 @@
 <?php
 
-    include('bdd.php');
+	include("bdd.php");
+    include("template.php");
 
-    class AppMVC {
 
-        private $bdd;
+	class AppMVC {
 
-        public function __construct(){
-            $this -> bdd = new Database();
-        }
+		private $bdd;
 
-        public function afficherPage($mapage){
-            if(!$this -> bdd -> getConnection()){
-                echo "Une erreur est servenue à la connexion";
-                return;
+		public function __construct() {
+			$this -> bdd = new BDD();
+		}
+
+		public function afficherPage($mapage, $id_question) {
+			if(!$this -> bdd -> connexion()) {
+				echo "Une erreur est survenue à la connexion";
+				return;
+			}
+			
+			if($mapage == 1) $this -> page1($id_question);
+			else if($mapage == 2) $this -> page2();
+			else $this -> page1($id_question);
+			
+			$this -> bdd -> deconnexion();
+		}
+
+		public function page1($id_question) {
+			$bonnereponse = $this -> bdd -> reponsecorrect($id_question);		
+            $reponses  = $this -> bdd -> getQuestion($id_question);
+
+            if (count($reponses) == 1){
+                
+                    foreach($reponses as $reponse) {
+				        echo '<h2>'."Question n°".$reponse -> id . " : " .$reponse -> intitule.'</h2>';
+			        }
+
+			    $reponses  = $this -> bdd -> getReponses($id_question);
+			
+                foreach($bonnereponse as $bonne){
+                    echo $bonne -> id;
+                }
+
+			    echo '<ul>';
+			    foreach($reponses as $reponse) {
+				    echo '<li><input type="radio" name="reponses" value="'.$reponse -> id.'"> '.$reponse -> intitule.'</li>';
+			    }
+			    echo '</ul>';	
             }
-
-            $resultats = $this->bdd->requete(); 
-
-        // Affiche les résultats ou fais quelque chose avec
-            foreach ($resultats as $row) {
-                echo "Numéro du quizz : " . $row["id"] . " - Catégorie : " . $row["nom"] . "<br>";
+            else{
+                echo '<p>Erreur</p>';
             }
+            	
+		}
 
-            if($mapage == 1) $this -> page1();
-            else if ($mapage == 2) $this -> page2();
-            else $this -> page1();
+		public function page2() {
+			$vue = new Template('template/question.html');
+            echo $vue -> getSortie();
+		}
 
-            $this -> bdd -> closeConnection();
+        public function bouton($question){
+            echo '<form method="get">';
+                echo '<input type="hidden" name="question" value='.$question.'>';
+                echo '<button type="submit">Question suivante</button>';
+            echo '</form>';
         }
 
-        public function page1(){
-            echo '<h1>Bienvenue sur le quizz</h1>';
+        function verifierreponse($id_question) {
+            //if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["reponses"])) {
+                //$reponse_utilisateur = $_POST["reponses"];
+            //}
+            $bonnereponse = reponsecorrect($id_question);
+            foreach($bonnereponse as $bonneReponse){
+                if ($bonneReponse == 0)
+                {
+                    echo '<h2>Bonne réponse</h2>';
+                }
+                else
+                {
+                    echo '<h2>Mauvaise réponse</h2>';
+                }
+            }
         }
 
-        public function page2(){
-            echo "Deuxième page";
-        }
-    }
+
+	}
 ?>
