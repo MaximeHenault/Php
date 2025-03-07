1d5ba489081622674d333d179c18933418cfe0b9
MaximeHenault <maxime.henault@lyceefulbert.fr>
2025-03-07 15:17:50 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,43 @@
-<?php
-    include('appmvc.php');
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <title>Quizzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz</title>
+</head>
+<body>
+    <header>
+        <p>Bienvenue sur le Quizz</p>
+    </header>
+    <nav>
+        <?php
+	    include('appmvc.php');
 
-    if(isset($_GET['page'])) $page = $_GET['page'];
-    else $page = 1;
+	    if(isset($_GET['page'])) $page = $_GET['page'];
+	    else $page = 1;
 
-    $appMVC = new AppMVC();
+	    $monapp = new AppMVC();
+	
+        if(isset($_GET['question'])) $question = $_GET['question'];
+        else $question = 1; 
 
-    $appMVC -> afficherPage($page);
-?>
+        $monapp -> afficherPage($page, $question);
+        ?>
+
+        <button type = "submit">Valider</button>
+
+        <?php
+        //$monapp -> verifierreponse($question);
+        $monapp -> bouton($question+1);
+        ?>
+
+        
+    </nav>
+    <footer>
+        <p>Pour plus de quizz -><p>
+    </footer>
+
+    <script src = "index.js"></script>
+</body>
+</html>
